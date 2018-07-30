d2eee9d8b3f7cd0bece27988b351366f00ebfc79
vaisali2129 <vaishalivarma93@gmail.com>
2018-07-30 14:23:39 -0500

    Add files via upload

---
+++ Assignments/A05/dungeon_02/js/level_01.js
@@ -0,0 +1,281 @@
+var level_01 = 
+{
+
+	preload: function () {
+
+	},
+	create: function () {
+		console.log("level_01.js");
+		this.animation_playing = false;
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		this.prevDir = '';	// holds sprites previous direction (left , right) so
+							// we can face the correct direction when using the 'idle' animation
+
+		// Adding the knight atlas that contains all the animations
+		this.player = game.add.sprite(game.camera.width / 2, game.camera.height / 2, 'knight_atlas');
+
+		// Add walking and idle animations. Different aninmations are needed based on direction of movement.
+		this.player.animations.add('walk_left', Phaser.Animation.generateFrameNames('Walk_left', 0, 8), 20, true);
+		this.player.animations.add('walk_right', Phaser.Animation.generateFrameNames('Walk_right', 0, 8), 20, true);
+		this.player.animations.add('idle_left', Phaser.Animation.generateFrameNames('Idle_left', 0, 9), 20, true);
+		this.player.animations.add('idle_right', Phaser.Animation.generateFrameNames('Idle_right', 0, 9), 20, true);
+		this.player.animations.add('run_right', Phaser.Animation.generateFrameNames('Run_right', 0, 9), 20, true);
+		this.player.animations.add('run_left', Phaser.Animation.generateFrameNames('Run_left', 0, 9), 20, true);
+		this.player.animations.add('dead', Phaser.Animation.generateFrameNames('Dead', 1, 10), 20, false);
+		this.player.animations.add('jump_left', Phaser.Animation.generateFrameNames('Jump_left', 0, 9), 15, true);
+		this.player.animations.add('jump_right', Phaser.Animation.generateFrameNames('Jump_right', 0, 9), 15, true);
+		this.player.animations.add('attack_left', Phaser.Animation.generateFrameNames('Attack_left', 0, 9), 20, false);
+		this.player.animations.add('attack_right', Phaser.Animation.generateFrameNames('Attack_right', 0, 9), 20, true);
+		this.player.animations.add('jumpattack_left', Phaser.Animation.generateFrameNames('JumpAttack_left', 0, 9), 20, true);
+		this.player.animations.add('jumpattack_right', Phaser.Animation.generateFrameNames('JumpAttack_right', 0, 9), 20, true);
+		this.player.animations.play('idle_left');
+
+		// turn physics on for player
+		game.physics.arcade.enable(this.player);
+
+		// set the anchor for sprite to middle of the view
+		this.player.anchor.setTo(0.5);
+
+		k = game.input.keyboard;
+		game.addPauseButton(game);
+
+		// flag to stop movement if character dies
+		this.alive = true;
+
+		// // timed event to start game over state
+		// game.time.events.add(Phaser.Timer.SECOND * 4, this.endGame(), this);
+	},
+
+	update: function () 
+	{
+			this.move();
+
+	},
+
+	endGame: function () {
+
+		game.state.start('gameOver');
+	
+	},
+
+	render: function()
+	{
+		game.debug.bodyInfo(this.player, 16, 24);
+		// Instructions:
+		game.debug.text( "Use arrow keys to move sprite around.", game.width/2, game.height-10 );
+	},
+	
+	move: function()
+	{
+		// Each key changes the players velocity in the x or y direction
+		// and plays the proper animation. It sets the prevDir so we can
+		// face the correct way when stopped.
+
+		// Walk left
+		if (k.isDown(Phaser.Keyboard.LEFT) && !k.isDown(Phaser.Keyboard.SHIFT))
+		{
+			if(k.isDown(Phaser.Keyboard.UP))
+			{
+				this.player.body.velocity.x = -200;
+				this.player.body.velocity.y = -200;
+			}
+			else if(k.isDown(Phaser.Keyboard.DOWN))
+			{
+				this.player.body.velocity.x = -200;
+				this.player.body.velocity.y = 200;
+			}
+			else{
+				this.player.body.velocity.x = -200;
+				this.player.body.velocity.y = 0;
+			}
+			this.player.animations.play('walk_left');
+			this.prevDir = 'left'
+		}
+
+		// Walk right
+		if (k.isDown(Phaser.Keyboard.RIGHT) && !k.isDown(Phaser.Keyboard.SHIFT)) 
+		{
+			if(k.isDown(Phaser.Keyboard.UP))
+			{
+				this.player.body.velocity.x = 200;
+				this.player.body.velocity.y = -200;
+			}
+			else if(k.isDown(Phaser.Keyboard.DOWN)){
+				this.player.body.velocity.x = 200;
+				this.player.body.velocity.y = 200;
+			}
+			else
+			{
+				this.player.body.velocity.x = 200;
+				this.player.body.velocity.y = 0;
+			}
+			this.player.animations.play('walk_right');
+			this.prevDir = 'right'
+		}
+
+		// Run left
+		if (k.isDown(Phaser.Keyboard.SHIFT) && k.isDown(Phaser.Keyboard.LEFT)) 
+		{
+			if(k.isDown(Phaser.Keyboard.UP))
+			{
+				this.player.body.velocity.x = -400;
+				this.player.body.velocity.y = -400;
+			}
+			else if(k.isDown(Phaser.Keyboard.DOWN))
+			{
+				this.player.body.velocity.x = -400;
+				this.player.body.velocity.y = 400;
+			}
+			else{
+				this.player.body.velocity.x = -400;
+				this.player.body.velocity.y = 0;
+			}
+			this.player.animations.play('run_left');
+			this.prevDir = 'left'
+		}
+
+		// Run right
+		if (k.isDown(Phaser.Keyboard.SHIFT) && k.isDown(Phaser.Keyboard.RIGHT)) 
+		{
+			if(k.isDown(Phaser.Keyboard.UP))
+			{
+				this.player.body.velocity.x = 400;
+				this.player.body.velocity.y = -400;
+			}
+			else if(k.isDown(Phaser.Keyboard.DOWN))
+			{
+				this.player.body.velocity.x = 400;
+				this.player.body.velocity.y = 400;
+			}
+			else{
+				this.player.body.velocity.x = 400;
+				this.player.body.velocity.y = 0;
+			}
+			this.player.animations.play('run_right');
+			this.prevDir = 'right'
+		}
+
+		// Walk up
+		if (k.isDown(Phaser.Keyboard.UP))
+		{
+			if(k.isDown(Phaser.Keyboard.LEFT))
+			{
+				this.player.body.velocity.x = -200;
+				this.player.animations.play('walk_left');
+
+			}
+			else if(k.isDown(Phaser.Keyboard.RIGHT))
+			{
+				this.player.body.velocity.x = 200;
+				this.player.animations.play('walk_right');
+			}
+			else{
+				this.player.body.velocity.x = 0;
+				if(this.prevDir == 'left'){
+					this.player.animations.play('walk_left');
+				}else{
+					this.player.animations.play('walk_right');
+				}
+			}
+			this.player.body.velocity.y = -200;
+		}
+
+		// Walk down
+		if (k.isDown(Phaser.Keyboard.DOWN))
+		{
+			if(k.isDown(Phaser.Keyboard.LEFT))
+			{
+				this.player.body.velocity.x = -200;
+				this.player.animations.play('walk_left');
+
+			}
+			else if(k.isDown(Phaser.Keyboard.RIGHT))
+			{
+				this.player.body.velocity.x = 200;
+				this.player.animations.play('walk_right');
+			}
+			else{
+				this.player.body.velocity.x = 0;
+				if(this.prevDir == 'left'){
+					this.player.animations.play('walk_left');
+				}else{
+					this.player.animations.play('walk_right');
+				}
+			}
+			this.player.body.velocity.y = 200;
+		}
+
+		// idle
+		if (!k.isDown(Phaser.Keyboard.LEFT) && !k.isDown(Phaser.Keyboard.RIGHT) && !k.isDown(Phaser.Keyboard.UP) 
+		&& !k.isDown(Phaser.Keyboard.DOWN) && !k.isDown(Phaser.Keyboard.SPACEBAR) && !k.isDown(65) 
+		&& !k.isDown(Phaser.Keyboard.ENTER) && !k.isDown(83) && this.alive)
+		{
+			if(this.prevDir == 'left'){
+				this.player.animations.play('idle_left');
+			}else{
+				this.player.animations.play('idle_right');
+			}
+			this.player.body.velocity.x = 0;
+			this.player.body.velocity.y = 0;
+		}
+		
+		// attack
+		if (k.isDown(65))
+		{
+			if (this.prevDir == 'left')
+			{
+				this.player.animations.play('attack_left')
+			}
+			else{
+				this.player.animations.play('attack_right')
+			}
+		}
+
+		// jump attack
+		if (k.isDown(83))
+		{
+			if (this.prevDir == 'left')
+			{
+				this.player.animations.play('jumpattack_left')
+			}
+			else{
+				this.player.animations.play('jumpattack_right')
+			}
+			//this.player.body.y -= 0.50;
+		}
+
+		// jump
+		if (k.isDown(Phaser.Keyboard.SPACEBAR)) 
+		{
+			if(this.prevDir == 'left')
+			{
+				this.player.animations.play('jump_left');
+			}
+			else
+			{
+				this.player.animations.play('jump_right');
+			}
+			
+			this.player.body.velocity.y = -900;
+			var anims = game.add.tween(this.player.animations);
+			anims.to({ x: 2, y: 2 }, 100, Phaser.Easing.Elastic.Out, true);
+			anims.start();
+			anims.onComplete.add(function () {
+				this.player.body.velocity.y = 1100;
+				anims.stop();
+			}, this);
+		}
+
+		// dead
+		if(k.isDown(Phaser.Keyboard.ENTER))
+		{
+		this.player.animations.play('dead');
+		this.alive = false;
+
+		// go to game over screen after the death animation plays
+		this.player.animations.currentAnim.onComplete.add(this.endGame, this);
+		}
+	},
+
+}
